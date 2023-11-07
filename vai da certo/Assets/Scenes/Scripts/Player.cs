02fa1fffe11c0b2f3c580c5474668561da30df3b
ERIK DAVISON DA SILVA PEREIRA <20221170150045@ifrn.local>
2023-11-07 16:42:08 -0300

    arrumando

---
+++ vai da certo/Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{ 
+    public int velocidade = 15;
+    public int forcaPulo;
+
+    private Rigidbody rb;
+
+    public bool noChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       TryGetComponent( out rb); 
+    }
+     
+    void OnCollisionEnter (Collision col){
+        if(col.gameObject.tag == "chao"){
+            noChao = true;
+        }
+    }
+    
+    // Update is called once per frame
+    
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3( h, 0, v);
+        rb.AddForce(direcao * Time.deltaTime, ForceMode.Impulse);
+
+        if(Input.GetKeyDown(KeyCode.Space) && noChao){
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+        if(transform.position.y <= -10){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+    }
+}
---
+++ vai da certo/Assets/Scenes/Scripts/Player2.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player2 : MonoBehaviour
+{ 
+    public int velocidade = 10;
+    public int forcaPulo = 7;
+
+    private Rigidbody rb;
+    private AudioSource source;
+    public AudioClip clipPulo, clipMoeda;
+
+    public bool noChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       TryGetComponent( out rb); 
+       TryGetComponent ( out source);
+    }
+     
+    void OnCollisionEnter (Collision col){
+        if(col.gameObject.tag == "chão"){
+            noChao = true;
+        }
+    }
+    
+    // Update is called once per frame
+    
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(h, 0, v) * velocidade);
+
+        if(Input.GetKeyDown(KeyCode.Space) && (noChao == true)){
+            source.PlayOneShot(clipPulo);
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+        if(transform.position.y <= -10){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+
+         if(transform.position.y <= -10){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+    }
+}
+
